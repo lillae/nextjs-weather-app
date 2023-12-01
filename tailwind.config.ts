5f73ce50ad025161a3a876c862d91e5816205079
llaenv <lillaev@yahoo.com>
2023-12-01 14:36:22 +0100

    initial

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#8FB3C7',
+				secondary: '#02FF40',
+				blue: {
+					1: '#142534',
+					2: '#91B7CC',
+					3: '#3893B1',
+					4: '#267AB9',
+					5: '#033670',
+					6: '#1F5B88',
+				},
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
