894db2090dd9a8d7b54e0e859503a2cdfbe298f9
llaenv <lillaev@yahoo.com>
2023-12-08 15:31:55 +0100

    refactored CityList and City

--- app/ui/search/city-list.tsx
+++ app/ui/search/city-list.tsx
@@ -1,5 +1,13 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 import { City } from '.';
+import { SaveButton } from '@/ui/elements/buttons';
 import { ICapital } from '@/lib/types/definitions';
+import { useAppDispatch } from '@/lib/redux/redux-hooks';
+import { addCapital } from '@/lib/redux/features/capitals/capitals-slice';
 
 const CityList = ({
 	cities,
@@ -8,12 +16,41 @@ const CityList = ({
 	cities: ICapital[] | undefined;
 	query: string;
 }) => {
+	const router = useRouter();
+	const dispatch = useAppDispatch();
+	const [isSelected, setIsSelected] = useState<boolean>(false);
+	const [activeId, setActiveId] = useState<string>('');
+
+	const clickHandler = (id: string) => {
+		setActiveId(id);
+		setIsSelected(!isSelected);
+	};
+
+	const saveHandler = (city: ICapital) => {
+		dispatch(addCapital(city));
+		setActiveId('');
+		setIsSelected(false);
+		router.push('/');
+	};
 	return (
 		<div className='w-full'>
 			{cities &&
-				cities
-					.slice(0, 8)
-					.map((city, i) => <City key={city.id} city={city} term={query} />)}
+				cities.slice(0, 8).map((city) => {
+					const check = isSelected && city.id === activeId;
+					return (
+						<div
+							key={city.id}
+							onClick={() => clickHandler(city.id)}
+							className={`${
+								check ? 'bg-blue-2/5' : ''
+							} w-full cursor-pointer text-blue-4 flex items-center justify-between p-2`}>
+							<City city={city} term={query} />
+							{check && (
+								<SaveButton onClick={() => saveHandler(city)}>Save</SaveButton>
+							)}
+						</div>
+					);
+				})}
 		</div>
 	);
 };
--- app/ui/search/city.tsx
+++ app/ui/search/city.tsx
@@ -1,50 +1,20 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-import { useAppDispatch, useAppSelector } from '@/lib/redux/redux-hooks';
-import {
-	addCapital,
-	selectCapitalIds,
-} from '@/lib/redux/features/capitals/capitals-slice';
+import { useAppSelector } from '@/lib/redux/redux-hooks';
+import { selectCapitalIds } from '@/lib/redux/features/capitals/capitals-slice';
 import { useHighlighter } from '@/lib/hooks/useHighlighter';
-import { SaveButton } from '@/ui/elements/buttons';
 import { ICapital } from '@/lib/types/definitions';
 
 const City = ({ city, term }: { city: ICapital; term: string }) => {
-	const router = useRouter();
-	const dispatch = useAppDispatch();
 	const selectedIds = useAppSelector(selectCapitalIds);
 	const filteredCities = !selectedIds.includes(city.id);
 	const [highlightTerm] = useHighlighter();
 
-	const [isSelected, setIsSelected] = useState<boolean>(false);
-	const [activeId, setActiveId] = useState<string>('');
-
-	const clickHandler = (city: ICapital) => {
-		setIsSelected(!isSelected);
-		setActiveId(city.id);
-	};
-
-	const saveHandler = (city: ICapital) => {
-		dispatch(addCapital(city));
-		setActiveId('');
-		router.push('/');
-	};
-
 	return (
 		<>
 			{filteredCities && (
-				<div
-					className={`${
-						isSelected ? 'bg-blue-2/5' : ''
-					} w-full cursor-pointer text-blue-4 flex items-center justify-between p-2`}
-					onClick={() => clickHandler(city)}>
+				<div>
 					<p>{highlightTerm(city.name, term)}</p>
-					{city.id === activeId && isSelected && (
-						<SaveButton onClick={() => saveHandler(city)}>Save</SaveButton>
-					)}
 				</div>
 			)}
 		</>
